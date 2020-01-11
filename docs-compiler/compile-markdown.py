43704010e358927b3ce46926b01dba9807c6b924
DerpyJish <joshuatiger1@gmail.com>
2020-01-11 22:07:31 +0000

    Improved doc compiler
    * Root content can now contain any type
    * Fixed bug where content items were not inserted into the root correctly
    * Argument and return value data types now display in markdown file without full path
    * Made description formatting more consistent
    * Fixed various bugs with tables
    * Fixed bug where names of return values were cut short by the content compiler
    * Replaced "implementation" status with incomplete tag
    * Renamed "arguments" and "returns" to "f-arguments" and "f-returns", as to indicate they are used for functions
    * Renamed "content" to "contents" as to remain consistent with other attribute names

--- docs-compiler/compile-markdown.py
+++ docs-compiler/compile-markdown.py
@@ -1,3 +1,4 @@
+import copy
 import json
 import re
 
@@ -6,89 +7,121 @@ f = open( 'full-content.json' , 'r' ) # This file is include in .git-ignore
 content = json.loads( f.read() ) # The json object representing all documented content
 f.close()
 
+# TEXT HANDLERS #
+
 # Function to convert a string from camel case to a capitalised string
 # e.g. "entityComponentSystem" -> "Entity Component System"
 def deCamelCase( s ):
     return re.sub( r'(.)([A-Z])',r'\1 \2',s).title()
 
-# Function to compile a system content item into markdown
-def compileSystem( systemName , systemItem ):
-    if ( systemItem['kind'] != 'system' ): raise RuntimeError( 'Cannot compile "' + systemName + '" as a system as it is if of type "' + systemItem['kind'] + '"' )
+# Function to create a markdown header
+def createHeader( text , level , deCamel=False ):
+    return '\n' + '#'*level + ' ' + (deCamelCase(text) if deCamel else text) + '\n'
 
-    functionsMd = '\n## Functions\n'
+# Function to format a multiline string into markdown
+def formatParagraphs( text ):
+    return re.sub( r'\n+' , '\n\n' , text ) + '\n'
+
+# Function to create a table of an object using the given mapping
+def createTable( obj , mapping ):
+
+    # Header row
+    markdown = '\n|'
+    for label in mapping: markdown += ' ' + label + ' |'
+    markdown += '\n| :--- | :--- | :--- |\n'
+
+    # Body rows
+    for row in obj:
+        markdown += '|'
+        for label in mapping: markdown += ' ' + row[ mapping[label] ] + ' |'
+        markdown += '\n'
+
+    return markdown
+
+# Function to create a table of function arguments or return values
+def createFunctionValuesTable( heading , obj , level=1 ):
+    table = copy.copy( obj )
+    for row in table:
+        row['name'] = '`' + row['name'] + '`'
+        row['type'] = '`' + row['type'][row['type'].rfind('/')+1:] + '`'
+
+    # markdown += createHeader( heading , level )
+    markdown = '\n**' + heading + '**\n'
+    markdown += createTable( table , {
+        'Data Type': 'type',
+        'Name': 'name',
+        'Description': 'description'
+    })
+
+    return markdown
+
+# COMPILE SYSTEM #
+def compileSystem( itemName , item , level=1 ):
+    if ( item['kind'] != 'system' ): raise RuntimeError( 'Cannot compile "' + itemName + '" as a system as it is if of kind "' + item['kind'] + '"' )
+
+    # Markdown sections
+    openingMd = createHeader( itemName , level , True )
+    functionsMd = createHeader( 'Functions' , level+1 )
+    classesMd = ''
     areFunctions = False
-    classesMd = '\n## Classes\n'
     areClasses = False
 
+    # Opening section
+    if ( 'description' in item ): openingMd += formatParagraphs( item['description'] )
+
     # Iterate through system content
-    if ( 'content' in systemItem ):
-        for itemName in systemItem['content']:
-            item = systemItem['content'][itemName]
-
-            # Item is a function
-            if ( item['kind'] == 'function' ):
-                areFunctions = True
-                functionsMd += compileFunction( itemName , item , 3 )
-
-            # Item is a class
-            elif ( item['kind'] == 'class' ):
-                areClasses = True
-
-                # Basic overview
-                classesMd += '\n### ' + itemName + '\n'
-                if ( 'description' in item ): classesMd += re.sub( r'\n+' , '\n\n' , item['description'] ) + '\n'
-
-                # Methods
-                if ( 'content' in item ):
-                    for subItemName in item['content']:
-                        subItem = item['content'][subItemName]
-                        if ( subItem['kind'] == 'function' ):
-                            classesMd += compileFunction( itemName + '.' + subItemName , subItem , 4 )
-                        else:
-                            raise RuntimeError( 'Cannot compile ' + subItem['kind'] + ' "' + subItemName + '" (content of system "' + itemName + '") as it is not a function (method)' )
-
-            # Item is of invalid kind
-            else:
-                raise RuntimeError( 'Cannot compile ' + item['kind'] + ' "' + itemName + '" (content of system "' + systemName + '") as it is not a function or class' )
+    if ( 'contents' in item ):
+        for childItemName in item['contents']:
+            childItem = item['contents'][childItemName]
+            if   ( childItem['kind'] == 'function' ): areFunctions = True; functionsMd += compileFunction( childItemName , childItem , level+2 )
+            elif ( childItem['kind'] == 'class' ):    areClasses = True;   classesMd += compileClass( childItemName , childItem , level+1 )
+            else: raise RuntimeError( 'Cannot compile ' + item['kind'] + ' "' + childItemName + '" (content of system "' + itemName + '") as it is not a function or class' )
 
     # Output final result
-    finalMd = '\n# ' + deCamelCase(systemName) + '\n'
-    if ( 'description' in systemItem ): finalMd += systemItem['description'] + '\n'
-    if ( areFunctions ): finalMd += functionsMd + '\n'
-    if ( areClasses ): finalMd += classesMd + '\n'
+    finalMd = openingMd
+    if ( areFunctions ): finalMd += functionsMd
+    if ( areClasses ): finalMd += classesMd
 
     return finalMd
 
-# Function to compile function content item into markdown
-def compileFunction( functName , functItem , level=1 ):
-    if ( functItem['kind'] != 'function' ): raise RuntimeError( 'Cannot compile "' + functName + '" as a function as it is if of type "' + functItem['kind'] + '"' )
+# COMPILE CLASS #
+def compileClass( itemName , item , level=1 ):
+    if ( item['kind'] != 'class' ): raise RuntimeError( 'Cannot compile "' + itemName + '" as a class as it is if of kind "' + item['kind'] + '"' )
 
     # Basic overview
-    finalMd = '\n' + '#'*(level) + ' ' + functName + '\n'
-    if ( 'description' in functItem ): finalMd += re.sub( r'\n+' , '\n\n' , functItem['description'] ) + '\n'
+    markdown = createHeader( itemName , level )
+    if ( 'description' in item ): markdown += re.sub( r'\n+' , '\n\n' , item['description'] ) + '\n'
 
-    # Arguments
-    if ( 'arguments' in functItem ):
-        # finalMd += '\n' + '#'*(level+1) + ' Arguments\n'
-        finalMd += '\n**Arguments**\n'
+    # Methods section
+    if ( 'contents' in item ):
+        for subItemName in item['contents']:
+            subItem = item['contents'][subItemName]
+            if ( subItem['kind'] == 'function' ): markdown += compileFunction( itemName + '.' + subItemName , subItem , level+1 )
+            else: raise RuntimeError( 'Cannot compile ' + subItem['kind'] + ' "' + subItemName + '" (content of system "' + itemName + '") as it is not a function (method)' )
 
-        finalMd += '\n| Data Type | Name | Description |\n| :--- | :--- | :--- |\n' # Blank line is required before a table so that GitHub can render it
-        for arg in functItem['arguments']: finalMd += '| `' + arg['type'] + '` | `' + arg['name'] + '` | ' + arg['description'] + ' |\n'
+    return markdown
 
-    # Return values
-    if ( 'returns' in functItem ):
-        # finalMd += '\n' + '#'*(level+1) + ' Returns\n'
-        finalMd += '\n**Returns**\n'
+# COMPILE FUNCTION #
+def compileFunction( itemName , item , level=1 ):
+    if ( item['kind'] != 'function' ): raise RuntimeError( 'Cannot compile "' + itemName + '" as a function as it is if of kind "' + item['kind'] + '"' )
 
-        finalMd += '\n| Data Type | Name | Description |\n| :--- | :--- | :--- |' # Blank line is required before a table so that GitHub can render it
-        for val in functItem['returns']: finalMd += '| `' + val['type'] + '` | `' + val['name'] + '` | ' + val['description'] + ' |\n'
+    # Basic overview
+    markdown = createHeader( itemName , level )
+    if ( 'description' in item ): markdown += formatParagraphs( item['description'] )
 
-    return finalMd
+    # Argument and return value tables
+    if ( 'f-arguments' in item ): markdown += createFunctionValuesTable( 'Arguments' , item['f-arguments'] )
+    if ( 'f-returns' in item ): markdown += createFunctionValuesTable( 'Return values' , item['f-returns'] )
+
+    return markdown
 
 # Compile all systems in the root content
 docMd = ''
-for systemName in content['content']:
-    docMd += compileSystem( systemName , content['content'][systemName] )
+for systemName in content['contents']:
+    itemKind = content['contents'][systemName]['kind']
+    if ( itemKind == "system" ): docMd += compileSystem( systemName , content['contents'][systemName] )
+    elif ( itemKind == "class" ): docMd += compileClass( systemName , content['contents'][systemName] )
+    elif ( itemKind == "function" ): docMd += compileFunction( systemName , content['contents'][systemName] )
 docMd = docMd.strip()
 
 # Save complete markdown to file
