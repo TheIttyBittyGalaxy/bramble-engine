c5408697dc98292693cc6bdc5a36e528db26ad43
DerpyJish <joshuatiger1@gmail.com>
2020-01-11 17:19:07 +0000

    Updated doc compiler
    * Code commented
    * Ability to define return values in doc strings added
    * Ability to define multi-line descriptions added
    * Additional tweaks

--- docs-compiler/compile-content.py
+++ docs-compiler/compile-content.py
@@ -1,42 +1,54 @@
 import json
 from pprint import pprint
 
-# Load inital content
+#    JavaScript doc string syntax
+#    /* parentContentPath
+#    Lines with no definition indicator (see following lines) are added to the content's decription
+#    Decription can be multi-line, and supports markdown
+#    argument: valueName valueType Markdown description of the argument
+#    return: valueName valueType Markdown description of the return value
+#    implementation: none/partial/complete
+#    */
+#
+#    Comment closer (*/) does not have to be on it's own line
+#    To indicate that method c belongs to class B of system a, the parent content path would be "a/B"
+
+# Load base content from file
 f = open( 'base-content.json' , 'r' )
-content = json.loads( f.read() )
+content = json.loads( f.read() ) # The json object representing all documented content
 f.close()
 
-# Parse code base for doc strings
+# List of files that should be searched for doc strings
 fileNames = [
-    # 'core/init.js',
-    # 'core/assets.js',
-    # 'core/state.js',
-    # 'core/loop.js',
-    # 'core/start.js',
-    # 'core/draw.js',
-    # 'core/input.js',
+    'core/init.js',
+    'core/assets.js',
+    'core/state.js',
+    'core/loop.js',
+    'core/start.js',
+    'core/draw.js',
+    'core/input.js',
 
-    # 'object/vector.js',
+    'object/vector.js',
     'object/entity.js',
-    # 'object/component.js',
+    'object/component.js',
 
-    # 'components.js',
+    'components.js',
 ]
 
-# For each file to be documented
+# For each file to be documented, load the file and scan each line looking for doc strings
 for fileName in fileNames:
+    parsing = False # Indicates if a doc string is currently being parsed
+    pLine = "" # The previous line of the file
+    item = {} # The item (e.g. class) currently being parsed
+    container = content # The container of the current item
+
     f = open( '../bramble/' + fileName , 'r' )
-    parsing = False
-    pLine = ""
 
-    item = {}
-    container = content
-
-    # For each line in the file
+    # Scan lines
     for line in f:
         sLine = line.strip()
 
-        # If currently parsing a doc string
+        # When parsing doc string
         if ( parsing ):
 
             # Check if parsing should finish
@@ -44,7 +56,7 @@ for fileName in fileNames:
                 parsing = False
                 sLine = sLine[:-2]
 
-            # Function argument
+            # Identify a function argument definition
             if ( sLine[:9] == 'argument:' ):
                 if ( 'arguments' not in item ): item['arguments'] = []
                 data = sLine[9:].strip()
@@ -59,41 +71,59 @@ for fileName in fileNames:
                     "description": description
                 })
 
-            # Implementation
+            # Identify a function return value definition
+            if ( sLine[:7] == 'return:' ):
+                if ( 'returns' not in item ): item['returns'] = []
+                data = sLine[9:].strip()
+                name = data[:data.find(' ')]
+                data = data[data.find(' ')+1:]
+                type = data[:data.find(' ')]
+                data = data[data.find(' ')+1:]
+                description = data
+                item['returns'].append({
+                    "name": name,
+                    "type": type,
+                    "description": description
+                })
+
+            # Identify an implementation definition
             elif ( sLine[:15] == 'implementation:' ):
                 item['implementation'] = sLine[15:].strip()
 
-            # Description
+            # Description definition
             else:
-                item['description'] = sLine
+                if ( 'description' not in item ): item['description'] = sLine
+                else: item['description'] += '\n' + sLine
 
-        # If a doc string has been started
+        # When starting new doc string
         elif ( sLine[:4] == '/*::' ):
-                parsing = True
-                item = {}
-                container = content
-                for contentName in sLine[4:].strip().split('/'):
-                    if ( 'content' not in container ): container['content'] = {}
-                    if ( contentName not in container['content'] ): container['content'][contentName] = {}
-                    container = container['content'][contentName]
+
+            # Reset parser
+            parsing = True
+            item = {}
+            container = content
+
+            # Navigate to new items's container
+            for contentName in sLine[4:].strip().split('/'):
+                if ( 'content' not in container ): container['content'] = {}
+                if ( contentName not in container['content'] ): container['content'][contentName] = {}
+                container = container['content'][contentName]
                 if ( 'content' not in container ): container['content'] = {}
 
-                # Set the name and kind of the current item
-                if ( pLine[:6] == 'class '):
-                    itemName = pLine[6:pLine.find(' ')-7]
-                    item['kind'] = 'class'
-                else: # Is a function/method
-                    itemName = pLine[:pLine.find('(')]
-                    item['kind'] = 'function'
+            # Set the name and kind of the current item based on the previous line
+            if ( pLine[:6] == 'class '):
+                itemName = pLine[6:pLine.find(' ')-7]
+                item['kind'] = 'class'
+            else: # Is a function/method
+                itemName = pLine[:pLine.find('(')]
+                item['kind'] = 'function'
+            container['content'][itemName] = item
 
-                print( itemName , item['kind'] )
-                container['content'][itemName] = item
-
-        # Remember the previous line in the next iteration
+        # Remember the previous line for the next iteration
         pLine = sLine
 
-# Test
-f = open( 'full-content.json' , 'w' )
-f.write( json.dumps(content , indent=2, sort_keys=True) )
+# Save complete content to file
+f = open( 'full-content.json' , 'w' ) # This file is include in .git-ignore. Run this script locally to see the result
+# f.write( json.dumps(content) )
+f.write( json.dumps(content , indent=2, sort_keys=True) ) # Replace line above with this one in order to pretty print JSON
 f.close()
-# print(json.dumps(content))
