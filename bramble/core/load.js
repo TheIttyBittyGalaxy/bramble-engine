3c92cf67d3758c366077b9209a53ec43706b16e2
JoshIsAStudent <joshua.ponte18@bathspa.ac.uk>
2020-02-10 14:26:33 +0000

    Reconfigured game start sequence
    Now the sequence is:
    * Load bramble
    * Load game
    * Load assets
    Once assets are loaded:
    * Start bramble
    * Start game
    
    Additionally, the default loading state will only be added to the stack if there are not any states on the stack after the game load functoin has been run

---
+++ bramble/core/load.js
@@ -0,0 +1,21 @@
+// BRAMBLE LOAD //
+bramble.load = function() {
+
+  // Load game
+  game.load();
+
+  // Append canvas to page
+  bramble.canvas.style.width = bramble.canvas.width * bramble.pixelSize + "px";
+  bramble.canvas.style.height = bramble.canvas.height * bramble.pixelSize + "px";
+  if ( bramble.pixelSize > 1 ) bramble.canvas.style.imageRendering = "crisp-edges";
+  document.body.appendChild( bramble.canvas );
+
+  // Load assets
+  if ( assets.loader.unloadedAssets.length > 0 ) {
+    assets.loader.loadAssets();
+    if ( game.state.stack.length == 0 ) game.state.start( new BrambleLoadingState );
+  } else {
+    bramble.start();
+  }
+  
+}
